6c5a48f9d5c7f831714d4b018cbe4111ff95590e
Ojidaemo <vit.martsinovich@gmail.com>
2023-08-17 18:07:52 +0300

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Vitali Martsinovich on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
